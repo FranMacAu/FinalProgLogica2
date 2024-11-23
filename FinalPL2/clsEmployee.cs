158e76ed10ddcb24e82d82493d694b4d2c90f9f1
Francisco Mac Auliffe <franmacauliffe@gmail.com>
2024-11-23 14:45:29 -0300

    Agregar archivos de proyecto.

---
+++ FinalPL2/clsEmployee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalPL2
+{
+    internal class clsEmployee
+    {
+        public string FileName = "HR_Employee.csv";
+        public struct Employee
+        {
+            int BusinessEntityID;
+            int NationalIDNumber;
+            string LoginID;
+            string OrganizationNode;
+            int OrganizationLevel;
+            string JobTitle;
+            DateTime BirthDate;
+            char MaritalStatus;
+            char Gender;
+            DateTime HireDate;
+            int SalariedFlag;
+            int VacationHours;
+            int SickLeaveHours;
+            int CurrentFlag;
+            string rowguid;
+            DateTime ModifiedDate;
+        }
+
+        private Employee[] Employees = new Employee[500];
+
+
+
+    }
+}
